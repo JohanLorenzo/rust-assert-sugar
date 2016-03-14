1780f89056271e9d64e9631270d1243d70217ca4
Johan Lorenzo <rust@johan.lrnz.fr>
2016-03-14 16:39:43 +0100

    Added new tests on assert_err

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 
 #[macro_export]
 macro_rules! assert_err {
-    ($left:ident , $right:expr) => ({
+    ($left:expr , $right:expr) => ({
         assert_eq!(&($left).unwrap_err(), &($right));
     })
 }
@@ -12,8 +12,32 @@ macro_rules! assert_err {
 
 #[cfg(test)]
 describe! asserts {
-    it "should work with Err" {
-        let err: Result<u32, &str> = Err("Error message");
-        assert_err!(err, "Error message");
+
+    describe! assert_err {
+        it "should work with identifiers" {
+            let err: Result<u32, &str> = Err("Error message");
+            assert_err!(err, "Error message");
+        }
+
+        it "should work with functions" {
+            fn f() -> Result<u8, String> {
+                Err("It failed".to_owned())
+            }
+
+            assert_err!(f(), "It failed");
+        }
+
+        it "should work with structs" {
+            struct Foo;
+
+            impl Foo {
+                pub fn bar(&self) -> Result<u8, String> {
+                    Err("Foo failed to bar".to_owned())
+                }
+            }
+
+            let foo = Foo;
+            assert_err!(foo.bar(), "Foo failed to bar");
+        }
     }
 }
